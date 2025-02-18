fc6b25d15209ebdd7cdc218f4d21dd3feb86d9f9
Frank Herchet <frank@herchet.net>
2025-02-18 20:45:10 +0100

    update smart mode

--- hass/hyper_smart_mode.py
+++ hass/hyper_smart_mode.py
@@ -14,76 +14,109 @@
 # You need HACS and pyscript installed
 # copy the following file to the pyscript folder
 #
-
 @state_trigger("sensor.shellypro3em_08f9e0e6cb74_total_active_power")
 @service
 def update_hyper_smart_mode():
-    # Smart-CT-Modus aktiv?
-    hyper2000_smart_ct = state.get("input_boolean.hyper2000_smart_ct") == "on"
-    log.info(f"hyper2000_smart_ct: {hyper2000_smart_ct}")
+    # Constants AC-Modi
+    AC_OUT, AC_IN = 2, 1
+    BYPASS_MODE_ON, BYPASS_MODE_OFF, BYPASS_MODE_AUTO = 2, 1, 0
     
-    # Konstante AC-Modi
-    BAT_DISCHARGING, BAT_CHARGING = 2, 1
+    # Smart-CT-Modus aktiv?
+    hyper2k_smart_mode = state.get("input_boolean.hyper2000_smart_ct") == "on"
+    hyper2k_smart_bypass_mode = state.get("input_boolean.hyper2k_smart_bypass_mode") == "on"
+    log.info(f"SMART MODE: {hyper2k_smart_mode} | BYPASS MODE: {hyper2k_smart_bypass_mode}")
+    hyper2k_soc = float(state.get("sensor.solarflow_electric_level"))
+    hyper2k_soc_max = float(state.get("sensor.hyper2k_soc_max"))
+    
+    # Sensordaten einlesen & umwandeln
+    total_power_to_grid = float(state.get("sensor.shellypro3em_08f9e0e6cb74_total_active_power"))
+    bat_ac_mode = int(state.get("sensor.hyper2k_ac_mode"))
+    ac_power_out = int(state.get("sensor.solarflow_output_home_power"))
+    ac_power_in = int(state.get("sensor.solarflow_grid_input_power"))
+    solar_input_power = int(state.get("sensor.solarflow_solar_input_power"))
+    bypass_mode = state.get("sensor.solarflow_pass_mode")
+    
+    old_ac_mode = "AC_MODE_IN" if bat_ac_mode == AC_IN else "AC_MODE_OUT"
+    if bypass_mode == BYPASS_MODE_ON:
+        old_ac_mode = "AC_MODE_BYPASS"
+        
+    log.info(f"old_ac_mode: {old_ac_mode} | bypass_mode: {bypass_mode}")
     
     # Offset-Werte abrufen
     charging_offset = int(float(state.get("input_number.hyper2k_charging_offset")))
     discharging_offset = int(float(state.get("input_number.hyper2k_discharging_offset")))
     log.info(f"Charging Offset: {charging_offset}, Discharging Offset: {discharging_offset}")
-
-    # Sensordaten einlesen & umwandeln
-    total_power_to_grid = float(state.get("sensor.shellypro3em_08f9e0e6cb74_total_active_power"))
-    bat_ac_mode = int(state.get("sensor.solarflow_ac_mode"))
-    bat_discharging = int(state.get("sensor.solarflow_output_home_power"))
-    bat_charging = int(state.get("sensor.solarflow_grid_input_power"))
     
     # Debug-Logging
     log.info(f"total_power_to_grid: {total_power_to_grid} W | "
-             f"bat_discharging: {bat_discharging} W | "
-             f"bat_charging: {bat_charging} W")
+             f"ac_power_out: {ac_power_out} W | "
+             f"ac_power_in: {ac_power_in} W")
     
-    # Berechnung neuer Werte
-    new_bat_discharging = new_bat_charging = 0
+    # calculate new values
+    new_ac_output_limit = new_ac_input_limit = 0
     new_ac_mode = bat_ac_mode
     
-    household_power_demand = total_power_to_grid + bat_discharging - bat_charging
+    household_power_demand = total_power_to_grid + ac_power_out - ac_power_in
     
     if household_power_demand > discharging_offset:
-        new_bat_discharging = int(household_power_demand - discharging_offset)
-        new_ac_mode = BAT_DISCHARGING
+        new_ac_output_limit = int(household_power_demand - discharging_offset)
+        new_ac_mode = AC_OUT
     else: 
-        new_bat_discharging = 0
+        new_ac_output_limit = 0
     
     if household_power_demand < charging_offset:
-        new_bat_charging = int(household_power_demand + charging_offset)
-        new_ac_mode = BAT_CHARGING
+        new_ac_input_limit = int(abs(household_power_demand) + charging_offset)
+        new_ac_mode = AC_IN
     else:
-        new_bat_charging = 0
-  
-    # Do not send out values higher than 1200
-    new_bat_discharging = min(new_bat_discharging, 1200)
-    new_bat_charging = min(new_bat_charging, 1200)
+        new_ac_input_limit = 0
+        
+    # If smart bypass mode is active - no discharging above solar_input_power allowed
+    if hyper2k_smart_bypass_mode and solar_input_power > discharging_offset:
+        log.info(f"new_bat_discharging will be limited to {solar_input_power} and AC Mode in CHARGING")
+        new_ac_output_limit = min(new_ac_output_limit, solar_input_power)
+        new_ac_mode = AC_IN
     
-    log.info(f"household_power_demand: {household_power_demand}W | new_bat_discharging: {new_bat_discharging} | new_bat_charging: {new_bat_charging}")
+    log.info(f"household_power_demand: {household_power_demand:.2f}W | new_ac_output_limit: {new_ac_output_limit:.2f} | new_ac_input_limit: {new_ac_input_limit:.2f}")
 
-    if hyper2000_smart_ct:
-        # Werte für die Batterie setzen
-        service.call("number", "set_value", entity_id="number.solarflow_set_output_limit", value=new_bat_discharging)
-        service.call("number", "set_value", entity_id="number.solarflow_set_input_limit_2", value=new_bat_charging)
+    if hyper2k_soc >= hyper2k_soc_max-2 and solar_input_power > discharging_offset:
+        log.info(f"Hyper2k SOC is at {hyper2k_soc}%, trying to enforce BYPASS_MODE")
+        new_ac_mode = AC_OUT
+        new_ac_input_limit = 0
+        new_ac_output_limit = discharging_offset
+        
+    # Do not send out values higher than 1200
+    new_ac_output_limit = min(new_ac_output_limit, 1200)
+    new_ac_input_limit = min(new_ac_input_limit, 1200)
 
-        # AC-Modus schalten
-        if new_ac_mode == BAT_CHARGING and bat_ac_mode == BAT_DISCHARGING:
-            log.info("Switching to Charging Mode")
-            service.call("switch", "turn_on", entity_id="switch.solarflow_ac_mode")
-        elif new_ac_mode == BAT_DISCHARGING and bat_ac_mode == BAT_CHARGING:
+    if hyper2k_smart_mode:
+        # Send new limits to Hyper2k              
+        service.call("number", "set_value", entity_id="number.solarflow_set_output_limit", value=new_ac_output_limit)
+        service.call("number", "set_value", entity_id="number.solarflow_set_input_limit_2", value=new_ac_input_limit)
+
+        # Set AC-Mode
+        if new_ac_mode == AC_IN and bat_ac_mode == AC_OUT:
+            if bypass_mode != 2:
+                log.info("Switching to Charging Mode")
+                service.call("switch", "turn_on", entity_id="switch.solarflow_ac_mode")
+                input_text.smart_mode_debug = "-> CHARGING"
+            else:
+                log.info(f"Switching to Charging Mode not possible due to bypass mode")
+        elif new_ac_mode == AC_OUT and bat_ac_mode == AC_IN:
             log.info("Switching to Discharging Mode")
             service.call("switch", "turn_off", entity_id="switch.solarflow_ac_mode")
+            input_text.smart_mode_debug = "-> DISCHARGING"
         else:
             log.info(f"Keeping AC Mode {bat_ac_mode}")
+            if bat_ac_mode == AC_IN:
+                input_text.smart_mode_debug = "CHARGING"
+            else:
+                input_text.smart_mode_debug = "DISCHARGING"
             
     else:
         log.info("Smart CT mode not active")
 
     log.info(f"AC Mode: {bat_ac_mode} → {new_ac_mode} | "
-             f"Charging: {bat_charging} → {new_bat_charging} | "
-             f"Discharging: {bat_discharging} → {new_bat_discharging}")
+             f"Charging: {ac_power_in} → {new_ac_input_limit} | "
+             f"Discharging: {ac_power_out} → {new_ac_output_limit}")
     log.info(f"### DONE ###")
+    log.info(f"")
